6b87257d3d3294a16ca675ddb80c24ee734ebee6
Alexis_Jhair <alexisvillarruelj14@gmail.com>
2024-09-14 12:34:42 -0500

    reciclyview1partede2

---
+++ app/src/main/java/dev/alexisvillarruel/fragments/ui/model/SongModel.kt
@@ -0,0 +1,9 @@
+package dev.alexisvillarruel.fragments.ui.model
+
+data class SongModel(
+    val id : Int,
+    val songName : String,
+    val album : String,
+    val songTime : String,
+    val countPlay: String,
+)
